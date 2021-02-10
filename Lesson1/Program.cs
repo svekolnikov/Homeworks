c6b0be72b708c563a393e21efa8192adf25d6308
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-02-10 23:59:04 +0300

    init

---
+++ Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как тебя зовут?"); //выводим в консоль вопрос
+            string name = Console.ReadLine();        // получаем строку с именем и сохраняем в переменную name 
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Now}"); //выводим строку с именем и текщей датой
+        }
+    }
+}
