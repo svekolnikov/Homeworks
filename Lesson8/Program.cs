11e928ff65f880154f40f280495ef26529a3faf9
Alexey Svekolnikov <alexey.svekolnikov@gmail.com>
2021-03-17 13:42:50 +0300

    +1 задание +2 задание

---
+++ Lesson8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MyLibrary;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            TaskManager taskManager = new TaskManager();
+            taskManager.PrintProcesses();
+            Console.WriteLine("Введите номер процесса который завершить:");
+            int pid = Int32.Parse(Console.ReadLine());
+            taskManager.KillProcessById(pid);
+            Console.WriteLine("Введите имя процесса который завершить:");
+            string procName = Console.ReadLine();
+            taskManager.KillProcessByName(procName);
+
+            //Task2
+            DateTimeLibrary.GreetingFromLibrary();
+            DateTimeLibrary.WhatTimeIn(Cities.London);
+            DateTimeLibrary.WhatTimeIn(Cities.LosAngeles);
+            DateTimeLibrary.WhatTimeIn(Cities.Moscow);
+            DateTimeLibrary.WhatTimeIn(Cities.Suzhou);
+        }
+    }
+}
